d2a54e6fef6adf77ebfb265ce889f517954ea283
OrangeLeafDev <70693681+OrangeLeafDev@users.noreply.github.com>
2022-06-26 09:50:26 +0800

    Update essentials.js

--- essentials.js
+++ essentials.js
@@ -1,33 +1,32 @@
-class splitString {
+class NitroBlock {
     getInfo() {
         return {
-            "id": "splitString",
-            "name": "SplitString",
-            "blocks": [{
-                    "opcode": "substringy",
-                    "blockType": "reporter",
-                    "text": "letters [num1] through [num2] of [string]",
-                    "arguments": {
-                        "num1": {
-                            "type": "number",
-                            "defaultValue": "2"
+            “id”: “NitroBlock”,
+            “name”: “NitroBlock”,
+            “blocks”: [{
+                    “opcode”: “substringy”,
+                    “blockType”: “reporter”,
+                    “text”: "letters  through  of “,
+                    ”arguments“: {
+                        ”num1“: {
+                            ”type“: ”number“,
+                            ”defaultValue“: ”2“
                         },
-                        "num2": {
-                            "type": "number",
-                            "defaultValue": "5"
+                        ”num2“: {
+                            ”type“: ”number“,
+                            ”defaultValue“: ”5“
                         },
-                        "string": {
-                            "type": "string",
-                            "defaultValue": "hello world"
+                        ”string“: {
+                            ”type“: ”string“,
+                            ”defaultValue“: ”hello world“
                         }
                     }
                 },
             }],
-        "menus": { //we will get back to this in a later tutorial
+        ”menus": { //we will get back to this in a later tutorial
         }
     };
     substringy({num1, num2, string}) {
         return string.substring(num1 - 1, num2);
     };
 }
-Scratch.extensions.register(new splitString());
