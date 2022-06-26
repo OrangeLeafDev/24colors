2bf5f2551649fcde6e51a3678c2da38a73030851
OrangeLeafDev <70693681+OrangeLeafDev@users.noreply.github.com>
2022-06-26 14:48:35 +0800

    Update essentials.js

--- essentials.js
+++ essentials.js
@@ -1,33 +1,35 @@
 class Colors24ESS {
-    constructor() {}
+    constructor() { }
     getInfo() {
         return {
-            "id": "Colors24ESS",
-            "name": "24Colors ESS",
-            "blocks": [{
-                    "opcode": "substringy",
-                    "blockType": "reporter",
-                    "text": "letters  through  of ",
-                    "arguments": {
-                        "num1": {
-                            "type": "number",
-                            "defaultValue": "2"
-                        },
-                        "num2": {
-                            "type": "number",
-                            "defaultValue": "5"
-                        },
-                        "string": {
-                            "type": "string",
-                            "defaultValue": "hello world"
+                "id": "Colors24ESS",
+                "name": "24Colors ESS",
+                "blocks": [
+                    {
+                        "opcode": "substringy",
+                        "blockType": "reporter",
+                        "text": "letters  through  of ",
+                        "arguments": {
+                            "num1": {
+                                "type": "number",
+                                "defaultValue": "2"
+                            },
+                            "num2": {
+                                "type": "number",
+                                "defaultValue": "5"
+                            },
+                            "string": {
+                                "type": "string",
+                                "defaultValue": "hello world"
+                            }
                         }
                     }
-                },
-            }],
-        "menus": { //we will get back to this in a later tutorial
+                ],
+                "menus": {} //we will get back to this in a later tutorial
         }
     };
-    substringy({num1, num2, string}) {
+
+    substringy({ num1, num2, string }) {
         return string.substring(num1 - 1, num2);
     };
 }
